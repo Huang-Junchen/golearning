732cd4dee1b8b2a95516399aebb15402e665a735
DreamBot <jh967@sussex.ac.uk>
2022-07-17 20:28:57 +0800

    add ch1 04 to 06

--- ch1/04-dup1.go
+++ ch1/04-dup1.go
@@ -11,12 +11,15 @@ func main() {
 	input := bufio.NewScanner(os.Stdin)
 
 	for input.Scan() {
+		if input.Text() == "eof" {
+			break
+		}
 		counts[input.Text()]++
 	}
 
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", line, n)
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
